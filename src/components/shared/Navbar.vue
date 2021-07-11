95e81dbb56dab5c96efa406328e035256311d301
Jhonatan Malara <jhomalex07@hotmail.com>
2021-07-11 04:35:03 -0500

    - Prototipado básico del Navbar con buscador. - Importada la librería FontAwesome para la iconografía. - Creado un JSON de productos.

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,22 +1,75 @@
 <template>
-  <div class="navbar">Hola</div>
+  <div class="navbar">
+    <div class="title">Ecommerce</div>
+    <search-box v-model:search-text="searchText" />
+    <div class="navbar-options">
+      <button class="account-button">My Account</button>
+      <button class="account-button">
+        <i class="fas fa-shopping-bag car-button"></i>
+      </button>
+    </div>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue';
+import SearchBox from '@/components/shared/SearchBox.vue';
 
 export default defineComponent({
-  //   setup() {}
+  components: { SearchBox },
+  setup() {
+    const searchText = ref('');
+
+    watch(searchText, (searchText) => {
+      console.log(searchText);
+    });
+
+    return { searchText };
+  }
 });
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/scss/_vars';
+
 .navbar {
-  width: 100%;
   background: white;
   --tw-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
     0 2px 4px -1px rgba(0, 0, 0, 0.06);
   box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
     var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+  height: 6rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  top: 0rem;
+}
+
+.navbar-options {
+  display: flex;
+  margin-right: 2rem;
+}
+
+.account-button {
+  background-color: white;
+  border: 0rem;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 600;
+  color: $text-color;
+}
+
+.car-button {
+  font-size: 20px !important;
+  margin-left: 1rem;
+}
+
+.title {
+  font-size: 20px;
+  font-weight: 700;
+  color: $text-color;
+  margin-left: 2rem;
 }
 </style>
---
+++ src/components/shared/SearchBox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search-box">
+    <i class="fa fa-search" style="font-weight: 600"></i>
+    <input placeholder="Buscar" class="search-input" @input="onWrite" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SearchBox',
+  props: { searchText: String },
+  emits: ['update:searchText'],
+  setup(props, { emit }) {
+    let text = '';
+    const onWrite = (val: InputEvent) => {
+      if (val.data === null) {
+        text = text.slice(0, text.length - 1);
+      } else {
+        text += val.data;
+      }
+      emit('update:searchText', text);
+    };
+
+    return { onWrite };
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.search-input {
+  border: 0px;
+  outline: none;
+  margin-left: 1rem;
+  font-size: 16px;
+  min-width: 40rem;
+}
+
+.search-box {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 2px solid #000;
+  border-radius: 0.3rem;
+  height: 3rem;
+  padding: 0rem 1rem;
+}
+</style>
