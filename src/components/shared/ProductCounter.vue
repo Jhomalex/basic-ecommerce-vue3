57ceee0605131279594ffb2f85fd3c4a6399db6a
Jazmin Guo <jguo@masterzon-mail.com>
2021-07-22 22:49:26 -0600

    Mejorar el componente counter

--- src/components/shared/ProductCounter.vue
+++ src/components/shared/ProductCounter.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="quantity">
-    <button class="minus-btn" type="button" @click="reduceItem()">
-      <span>-</span>
-    </button>
-    <input type="text" v-bind:value="count" readonly />
-    <button class="plus-btn" type="button" v-on:click="addItem()">
-      <span>+</span>
-    </button>
+  <div
+    class="product-card-counter"
+    :style="[count === 0 ? { display: 'none' } : { display: '' }]"
+  >
+    <span class="product-card-cart-button">
+      <span class="icon counter-btn" @click="reduceItem()">-</span>
+      <span class="label counter-value">{{ count }}</span>
+      <span class="icon counter-btn" v-on:click="addItem()">+</span>
+    </span>
+  </div>
+  <div
+    class="product-card-counter"
+    :style="[count !== 0 ? { display: 'none' } : { display: '' }]"
+  >
+    <span class="product-card-cart-button add-product" @click="addItem()">
+      <span class="label">Agrgar</span>
+      <span class="icon icon-product-add">+</span>
+    </span>
   </div>
 </template>
 
@@ -32,7 +42,7 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.quantity {
+.product-card-counter {
   padding-top: 20px;
   margin-right: 60px;
   margin: auto;
@@ -40,37 +50,74 @@ export default defineComponent({
   padding: 10px;
 }
 
-.quantity input {
-  -webkit-appearance: none;
-  border: none;
-  text-align: center;
-  width: 100px;
-  font-size: 16px;
-  color: #43484d;
-  font-weight: 300;
+.product-card-cart-button {
+  height: 35px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  width: 100%;
+  top: 0;
+  left: 0;
+  cursor: pointer;
+  border-radius: 6px;
+  overflow: hidden;
+  background-color: #f3f3f3;
+  z-index: 1;
+  -webkit-transition: 0.15s ease-in-out;
+  -o-transition: 0.15s ease-in-out;
+  transition: 0.15s ease-in-out;
 }
 
-.quantity input:focus {
-  outline: none;
+.product-card-cart-button .label {
+  -webkit-box-flex: 1;
+  -ms-flex-positive: 1;
+  flex-grow: 1;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  font-size: 13px;
+  font-weight: 600;
 }
 
-.plus-btn {
-  margin-top: 2px;
-  border: #43484d;
+.product-card-cart-button .icon {
+  width: 35px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  background-color: #e6e6e6;
+  -webkit-transition: 0.15s ease-in-out;
+  -o-transition: 0.15s ease-in-out;
+  transition: 0.15s ease-in-out;
+}
+
+.product-card-cart-button .counter-btn {
   background-color: #000000;
   color: #ffffff;
-  cursor: pointer;
-  padding: 5px 10px;
-  border-radius: 2px;
 }
 
-.minus-btn {
-  margin-bottom: 3px;
-  border: #43484d;
+.product-card-cart-button .counter-value {
+  background-color: #ffffff;
+}
+
+.add-product:hover {
   background-color: #000000;
   color: #ffffff;
-  cursor: pointer;
-  padding: 5px 10px;
-  border-radius: 2px;
+}
+
+.product-card-cart-button:hover .icon-product-add {
+  background-color: #000000;
+  color: #ffffff;
 }
 </style>
